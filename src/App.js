731f5225461fc47948bbe0dda0d3e69163b0b1d7
Abhi Shah <116759626+asbee03@users.noreply.github.com>
2023-02-12 21:21:13 -0500

    card

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<>
+    <div class="card">
+      <div class="lecard">
+        <h2>Baljeet: The IT GOD</h2>
+        <img src="https://i.redd.it/zckxgrywjpk61.jpg" alt="Baljeet" />
+        <p class="details">Baljeet can solve any IT problem you have </p>
+      </div>
     </div>
+  <div class="buttons">
+  
+<button id="duplicate" class="outsideBtn">Duplicate</button>
+<button id="bg" class="outsideBtn">Background</button>
+<button id="header" class="outsideBtn">Heading</button>
+<button id="delete" class="outsideBtn">Delete</button>
+  
+  </div>
+  
+</>
   );
 }
 
